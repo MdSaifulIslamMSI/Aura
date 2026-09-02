edbd5f87ca75b182ac9c315bd7c03bc3d0c3a812
MdSaifulIslamMSI <mdsaifulislam38msi@gmail.com>
2026-09-02 19:03:07 +0530

    test(server): compile ESM-only svix for Jest via scoped babel transform

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,6 +3,12 @@ module.exports = {
     verbose: true,
     maxWorkers: '50%',
     workerIdleMemoryLimit: process.env.JEST_WORKER_IDLE_MEMORY_LIMIT || '512MB',
+    // svix v2 is ESM-only; compile just its entry to CJS for the CommonJS Jest
+    // runtime. All other node_modules keep default (untransformed) resolution.
+    transform: {
+        '^.+\\.[cm]?js$': ['babel-jest', { presets: [['@babel/preset-env', { targets: { node: 'current' } }]] }],
+    },
+    transformIgnorePatterns: ['node_modules/(?!(svix)/)'],
     setupFilesAfterEnv: ['./tests/setup.js'],
     testMatch: ['**/*.test.js'],
     coverageProvider: 'v8',
